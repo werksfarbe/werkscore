749e6347123256e7740bcb1333d85f881704c21a
tom <tom@werksfarbe.com>
2023-11-10 13:08:06 +0100

    plugindatei

---
+++ werkscore.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WerksCore
+Plugin URI: werksfarbe.com
+Description: Basis für Werksfarbe Plugins.
+Version: 0.1
+Author: Sonja+Tom
+Author URI: https://werksfarbe.com/
+License: -
+Text Domain: werkscore-plugin
+*/
