78e176ebe41efa7a8d2a7ec6e1f37468d1d45b43
tom <tom@werksfarbe.com>
2023-12-07 23:08:11 +0100

    farbwechsel finish

--- includes/class-color-change-section.php
+++ includes/class-color-change-section.php
@@ -12,7 +12,7 @@ function werkscore_add_data_attribute_to_vc_row_if_enabled($output, $obj, $atts)
 			if (!empty($atts['color_name'])) {
 				// Hinzufügen des data-Attributs
 				$color_name = esc_attr($atts['color_name']);
-				$output = preg_replace('/<section/', '<section data-colorchangto="' . $color_name . '"', $output, 1);
+				$output = preg_replace('/<section(\s+class="[^"]*)"/', '<section$1 panel" data-colorchangto="' . esc_attr($color_name) . '"', $output, 1);
 			}
 		}
 	}
@@ -21,15 +21,39 @@ function werkscore_add_data_attribute_to_vc_row_if_enabled($output, $obj, $atts)
 add_filter('vc_shortcode_output', 'werkscore_add_data_attribute_to_vc_row_if_enabled', 10, 3);
 
 
-// Frontend CSS
-function werkscore_enqueue_section_color_change_style() {
+function werkscore_enqueue_section_color_change_script() {
 	// Abrufen der Plugin-Einstellungen
 	$options = get_option('blocklink_option');
 
 	// Überprüfen, ob die "Section-Farbwechsel aktivieren"-Option aktiviert ist
 	if (!empty($options['section_color_change']) && $options['section_color_change'] == 1) {
+		// Pfad zu Ihrem Plugin-Verzeichnis
+		$plugin_url = plugin_dir_url(__FILE__);
 
-		wp_enqueue_style('werkscore-section-color-change', plugin_dir_url(dirname(__FILE__)) . 'public/styles/sectioncolorto.css');
+		// Einreihen des JavaScripts
+		wp_enqueue_script('werkscore-section-color-change', plugin_dir_url(dirname(__FILE__)) . 'public/scripts/sectioncolorto.js', array('jquery'), null, true);
 	}
 }
-add_action('wp_enqueue_scripts', 'werkscore_enqueue_section_color_change_style');
+add_action('wp_enqueue_scripts', 'werkscore_enqueue_section_color_change_script');
+
+
+function werkscore_generate_custom_color_classes() {
+	// Abrufen der Plugin-Einstellungen
+	$options = get_option('blocklink_option');
+
+	// Überprüfen, ob die "Section-Farbwechsel aktivieren"-Option aktiviert ist
+	if (isset($options['section_color_change']) && $options['section_color_change'] == 1) {
+		// Überprüfen, ob es benutzerdefinierte Farben gibt
+		if (isset($options['custom_colors']) && is_array($options['custom_colors'])) {
+			echo '<style>';
+			// Hinzufügen des grundlegenden CSS-Befehls
+			echo '.l-canvas { transition: background-color 1s ease; }';
+			foreach ($options['custom_colors'] as $color) {
+				// Generieren der CSS-Klasse für jede Farbe
+				echo '.color-' . sanitize_title($color['name']) . ' { background-color: ' . esc_attr($color['value']) . '; }';
+			}
+			echo '</style>';
+		}
+	}
+}
+add_action('wp_head', 'werkscore_generate_custom_color_classes');
