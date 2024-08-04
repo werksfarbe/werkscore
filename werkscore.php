a34ccd7e37fa8b64825a96e16c8e3089175007ca
Tom <tom@macbook-pro-von-tom.local>
2024-08-04 13:27:46 +0200

    test

--- werkscore.php
+++ werkscore.php
@@ -3,7 +3,7 @@
 Plugin Name: WerksCore Addon
 Plugin URI: 
 Description: Basis für ImprezaCore Addons.
-Version: 0.6.5
+Version: 0.6.6
 GitHub Plugin URI: https://github.com/werksfarbe/werkscore
 GitHub Branch: main
 License: -
