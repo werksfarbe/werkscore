b6d6679ccd7889a12c183e7675aafe7e76164c79
Tom <tom@macbook-pro-von-tom.local>
2024-08-04 11:20:02 +0200

    Neue Rolle für Solid WP Backups

--- werkscore.php
+++ werkscore.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: WerksCore
-Plugin URI: werksfarbe.com
-Description: Basis für Werksfarbe Plugins.
-Version: 0.5
-Author: Werksfarbe
-Author URI: https://werksfarbe.com/
+Plugin Name: WerksCore Addon
+Plugin URI: 
+Description: Basis für ImprezaCore Addons.
+Version: 0.6
+Author: 
+Author URI: /
 License: -
 Text Domain: werkscore-plugin
 */
@@ -23,3 +23,5 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/class-parallax-flipbox.php'
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-color-change-section.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-remove-gridoptions.php';
 
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-wfsuperadmin-role.php';
+register_activation_hook(__FILE__, array('WFSuperAdminRole', 'create_role'));
